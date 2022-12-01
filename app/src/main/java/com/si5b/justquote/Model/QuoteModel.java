bf474d01a7191497a4018c640219650e3d053387
radit <radityaa.pr@gmail.com>
2022-12-01 13:09:58 +0700

    crea model and package

---
+++ app/src/main/java/com/si5b/justquote/Model/QuoteModel.java
@@ -0,0 +1,14 @@
+package com.si5b.justquote.Model;
+
+public class QuoteModel {
+
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
